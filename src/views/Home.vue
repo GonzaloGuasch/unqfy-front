ac1bee139daf49ddb4e15cc3c4b94423a83c9886
GonzaloGuasch <gonzaloguasch@outlook.com>
2019-06-09 22:40:54 -0300

    laburo del dia sagrado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="logo-container">
+      <img src="../assets/unq.svg">
+      <img src="../assets/fy.svg">
+    </div>
+    <div class="search-bar">
+      <dropdown :options="options"
+                :selected="selectedOption"
+                @updateOption="updateSelected"
+                :placeholder="''">
+      </dropdown>
+      <form @submit.prevent="search">
+        <input v-model="query" type="text">
+        <button class="search-button">Search</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import dropdown from 'vue-dropdowns';
+
+const options = [
+          {
+            name: 'Artists',
+            searchFor: (router: any, searchInput: string) => router.push({name: 'search-results', params: {type: 'artists', query: searchInput}}),
+          },
+          {
+            name: 'Albums',
+            searchFor: (router: any, searchInput: string) => router.push({name: 'search-results', params: {type: 'albums', query: searchInput}}),
+          },
+        ]
 
 export default Vue.extend({
   name: 'home',
   components: {
-    HelloWorld,
+    dropdown,
+  },
+  data() {
+    return {
+      options,
+      selectedOption: options[0],
+      query: '',
+    }
+  },
+  methods: {
+    search() {
+      this.selectedOption.searchFor(this.$router, this.query)
+    },
+    updateSelected(payload: any) {
+      this.selectedOption = payload;
+    }
   },
 });
 </script>
+
+<style lang="scss">
+
+  .home {
+    background: black;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .search-bar {
+    margin-top: 100px;
+
+    .search-button {
+      background: #1ed760;
+      border: 0;
+      height: 29px;
+      border-radius: 3px;
+      cursor: pointer;
+    }
+  }
+
+</style>
---
+++ src/views/SearchResults.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div v-if="type === 'artist'">
+            ARTISTAS
+        </div>
+        <div v-else>
+            ALBUMES
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+
+    export default Vue.extend({
+        name: "search-results",
+        components: {
+        },
+        data() {
+            return {
+                type: '',
+                query: '',
+            };
+        },
+        async beforeCreate() {
+
+        },
+        created() {
+            this.type = this.$route.query.type as string
+            this.query = this.$route.query.query as string
+        }
+    });
+</script>
+
+<style lang="scss">
+    .container {
+        background: black;
+        height: 100%;
+        width: 100%;
+    }
+</style>
